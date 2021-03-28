dde72b3e73bb94b042e64a42c2b613acd3ecf535
Vovan1608 <vovan1608@gmail.com>
2021-03-28 23:11:52 +0300

    added #15_Command

---
+++ #15_Command/main.js
@@ -0,0 +1,25 @@
+// !Инкапсулирует вызов метода в один объект.
+// Command Pattern //
+// Пример из:
+// https://addyosmani.com/resources/essentialjsdesignpatterns/book/#commandpatternjavascript
+(function(){
+  var carManager = {
+    // Запросить информацию
+    requestInfo: function( model, id ){
+      return "The information for " + model + " with ID " + id + " is foobar";
+    },
+    // Купить машину
+    buyVehicle: function( model, id ){
+      return "You have successfully purchased Item " + id + ", a " + model;
+    },
+    // Организовать просмотр
+    arrangeViewing: function( model, id ){
+      return "You have successfully booked a viewing of " + model + " ( " + id + " ) ";
+    }
+  };
+})();
+carManager.execute = function ( name ) {
+  return carManager[name] && carManager[name].apply( carManager, [].slice.call(arguments, 1) );
+};
+carManager.execute( "arrangeViewing", "Ferrari", "14523" );
+carManager.execute( "requestInfo", "Ford Mondeo", "54323" );
